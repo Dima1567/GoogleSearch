2083f811177c4652c6eaf5ad0a2759c45f5e3a73
Dima1567 <dima.sava1567@gmail.com>
2021-12-29 13:46:04 +0300

    Initial Commit

---
+++ Google_Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google_Search
+//
+//  Created by Dima Savitskiy on 29.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
